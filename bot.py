7a675c3cf766fbbed9f3dc7acf9ed1797946a29d
David Yu <pppsdavid@gmail.com>
2020-01-26 22:08:18 -0500

    update bot to taken .env token

--- bot.py
+++ bot.py
@@ -3,12 +3,11 @@ import os
 import discord
 from discord.ext import commands
 
-# from dotenv import load_dotenv
-# load_dotenv()
-# token = os.getenv('DISCORD_TOKEN')
-
-# Bot token
-token = "NjcxMDUyMTU1NDY5NDMwODE0.Xi3_5A.l1uVgS2ERKj95I1UfNC8il9B1BA"
+#bot token
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+# print(token)
 
 # Initiate Bot instance
 bot = commands.Bot(command_prefix='!')
